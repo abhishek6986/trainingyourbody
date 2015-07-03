21afd7d8bf6e803fa5694d8099fb722fc4df2ebf
Abhishek Sinha <the.abhishek.6986@gmail.com>
2015-07-04 05:12:10 +0530

    final
    all done 4-7-2015

--- footer.php
+++ footer.php
@@ -25,15 +25,6 @@
                 		<h3>Useful Links</h3>	
 
                         <?php  wp_nav_menu(array('menu'=>'Main Menu','menu_class' => 'footer_menu col-md-8 pull-left')); ?>
-                       
-<!--
-						<ul>
-							<li><a rel="nofollow" target="_blank" href="#">Facebook</a>
-							<li><a rel="nofollow" target="_blank" href="#">Youtube</a>
-						</ul>
--->
-
-                		
 
                 </div>
 
@@ -43,25 +34,52 @@
 
                		<h3>Newsletter</h3> 
 
-                    <p>Suscribe to our ahasellus sit 
 
-amet justo sapien and raesent 
 
-bibendum you will get nim non 
-
-fringilla vestibulum.</p>
-
-					<form action="#">
-
-                    	<input type="text" name="your_name" placeholder="Your Name" id="">
-
-                        <input type="text" name="your_email" placeholder="Your Email" id="">
-
-                    </form>	
-
+					<script type="text/javascript">
+                    //<![CDATA[
+                    if (typeof newsletter_check !== "function") {
+                    window.newsletter_check = function (f) {
+                        var re = /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-]{1,})+\.)+([a-zA-Z0-9]{2,})+$/;
+                        if (!re.test(f.elements["ne"].value)) {
+                            alert("The email is not correct");
+                            return false;
+                        }
+                        if (f.elements["nn"] && (f.elements["nn"].value == "" || f.elements["nn"].value == f.elements["nn"].defaultValue)) {
+                            alert("The name is not correct");
+                            return false;
+                        }
+                        for (var i=1; i<20; i++) {
+                        if (f.elements["np" + i] && f.elements["np" + i].required && f.elements["np" + i].value == "") {
+                            alert("");
+                            return false;
+                        }
+                        }
+                        if (f.elements["ny"] && !f.elements["ny"].checked) {
+                            alert("You must accept the privacy statement");
+                            return false;
+                        }
+                        return true;
+                    }
+                    }
+                    //]]>
+                    </script>                    
+                    <form method="post" action="http://www.trainingyourbody.dev/wp-content/plugins/newsletter/do/subscribe.php" onsubmit="return newsletter_check(this)">
                     
+                        <input class="newsletter-firstname" type="text" name="nn" placeholder="Your Name" size="30"required>
+                        <input class="newsletter-email" type="email" name="ne" placeholder="Your Email" size="30" required>
+                        <input class="newsletter-submit" type="submit" value="Subscribe"/>
+                    </form>
 
-                    <a href="" class="read_more">Read More</a>
+				<div class="clear"></div>
+				<h3>Follow Us</h3> 
+                <div class="social_icon"> 
+                    <a href="<?php echo get_option_tree( 'facebook_link'); ?>" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/img/social_icon_1.png" alt=""></a>
+                    <a href="<?php echo get_option_tree( 'instagram_link'); ?>" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/img/social_icon_5.png" alt=""></a>
+                    <a href="<?php echo get_option_tree( 'youtube_link'); ?>" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/img/social_icon_6.png" alt=""></a>
+                    <a href="<?php echo get_option_tree( 'twitter_link'); ?>" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/img/social_icon_2.png" alt=""></a>
+                    <a href="<?php echo get_option_tree( 'google_plus_link'); ?>" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/img/social_icon_3.png" alt=""></a>
+                </div>
 
                 </div> 
 
@@ -81,7 +99,7 @@ fringilla vestibulum.</p>
 
             		<a href="" class="col-md-3 pull-left"><img class="footer_logo_img" src="<?php echo get_template_directory_uri(); ?>/img/footer_logo.png" alt=""></a>
 
-                    <p class="col-md-10 copy_right pull-right">&copy; <a href="http://trainingyourbody.com">Training your Body</a> |    Design: <a href="https://www.wodumedia.com" target="_blank">Wodu Media</a> Hosting: <a href="https://www.keywestwebdesign.com" target="_blank">Key West Web Design</a></p>
+                    <p class="col-md-10 copy_right pull-right">&copy; <?php echo date('Y'); ?> <a href="http://trainingyourbody.com">Training your Body</a> |    Design: <a href="https://www.wodumedia.com" target="_blank">Wodu Media</a> Hosting: <a href="https://www.keywestwebdesign.com" target="_blank">Key West Web Design</a></p>
 
             </div>
 
--- page.php
+++ page.php
@@ -1,8 +1,10 @@
 <?php 	get_header();	$page_id = get_the_ID();	$my_wp_query = new WP_Query();	$all_wp_pages = $my_wp_query->query(array('post_type' => 'page'));	$page_parent = $post->post_parent; ?>
 		<?php if (have_posts()): while (have_posts()) : the_post(); ?>
-		<div class="container page_content">
+		<div class="container-fluid inner-page">
+        <div class="container page_content">
         		<h2 class="page_title"><?php the_title(); ?></h2>
 			<?php the_content(); ?>   			<?php		$child_pages = get_page_children($page_id,$all_wp_pages);		if (  is_page(array(227,252)) && is_array($child_pages) && count($child_pages) >  0 ){			$pagesP = $child_pages;			$pagesP = array_reverse($pagesP);		}				if ( $page_parent == 227 || $page_parent == 252 ){			$child_pages = get_page_children($page_parent,$all_wp_pages);					$pagesP = $child_pages;			$pagesP = array_reverse($pagesP);						$page_id = $page_parent;		}?><?php if ( is_array($pagesP) && count($pagesP) > 0 ){ $i=2; ?><ul class="pagination">	<li class="<?php echo get_the_ID() == $page_id ? 'active' : '' ?>"><a href="<?php echo get_permalink($page_id); ?>">1</a></li>  <?php foreach($pagesP as $p): ?>	<li class="<?php echo get_the_ID() == $p->ID ? 'active' : '' ?>"><a href="<?php echo get_permalink($p); ?>"><?php echo $i ?></a></li>  <?php $i++; endforeach; ?></ul>			<?php } ?>
+        </div>
         </div>
 		<?php endwhile; ?>
 		<?php endif; ?>
